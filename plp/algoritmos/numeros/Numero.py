1e27be118385ed222c191684023fdeb31f7c7b03
Wesley <wesleyadriann@gmail.com>
2020-04-28 09:49:56 -0300

    Adds class numero
    Signed-off-by: Wesley <wesleyadriann@gmail.com>

--- plp/algoritmos/numeros/Numero.py
+++ plp/algoritmos/numeros/Numero.py
@@ -3,7 +3,26 @@
 
 class Numero():
     def __init__(self, numero):
-        self.__numero = numero
+        if(numero >= 1 and numero <= 999)
+            self.__numero = numero
 
-    def numero_extenso(self):
+    def _extenso(self):
+        pass
+
+    def _getUnidade(self):
+        return self.__numero[-1]
+
+    def _getDezena(self):
+        return self.__numero[1]
+
+    def _getCentena(self):
+        return self.__numero[0]
+
+    def _unidadeExtenso(self):
+        pass
+
+    def _dezenaExtenso(self):
+        pass
+
+    def _centenaExtenso(self):
         pass
