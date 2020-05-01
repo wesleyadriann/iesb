2b74f748fbcbd7d17fbc01b4122cb4072678da7d
Wesley <wesleyadriann@gmail.com>
2020-04-30 22:55:49 -0300

    Adds tabela_dinamica funcstions
    Signed-off-by: Wesley <wesleyadriann@gmail.com>

--- analise_de_algoritmos/tabela_dinamica/tabela_dinamica.c
+++ analise_de_algoritmos/tabela_dinamica/tabela_dinamica.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include "omp.h"
 
 #define tam 100000000
@@ -15,6 +14,10 @@ typedef struct tabela_dinamica {
 } tabela_dinamica;
 
 tabela_dinamica * aloca_tabela();
+long int * aloca_vetor(long int qtd) ;
+void copia (long int * vetor1, long int *  vetor2, long int qtd);
+void incluir(tabela_dinamica * tabela, int x);
+void remover(tabela_dinamica * tabela);
 
 int main() {
   int i = 0;
@@ -24,16 +27,19 @@ int main() {
   tabela1 = aloca_tabela();
 
 
-  tempo_inicial =  omp_get_wtime();
+  tempo_inicial = omp_get_wtime();
   for(i = 0; i < execucoes; i++) {
     double tempo_inicial_execucao, tempo_final_execucao = 0.0;
     int j = 0;
-    for(j=0; j < execucoes; j++) {
-
+    tempo_inicial_execucao =  omp_get_wtime();
+    for(j=0; j < tam; j++) {
+      incluir(tabela1, 1);
     }
+    tempo_final_execucao = omp_get_wtime();
+    printf("Execução %d , tempo %lf \n", i, tempo_final_execucao - tempo_inicial_execucao);
   }
   tempo_final = omp_get_wtime();
-  printf("%f %f", tempo_final, tempo_inicial);
+  printf("Execução total, tempo %lf", tempo_final - tempo_inicial);
 
   return 0;
 };
@@ -45,5 +51,50 @@ tabela_dinamica * aloca_tabela() {
   nova_tabela->qtd = 0;
   nova_tabela->tamanho = 0;
   nova_tabela->vet = 0;
+
   return nova_tabela;
+}
+
+void incluir(tabela_dinamica * tabela, int x) {
+  long int i = 0;
+
+  if(tabela->tamanho==0) {
+    tabela->vet = aloca_vetor(1);
+    tabela->tamanho = 1;
+  }
+
+  if(tabela->tamanho == tabela->qtd) {
+    long int * vetor_aux = NULL;
+
+    vetor_aux = aloca_vetor(multiplicador * tabela->tamanho);
+    copia(tabela->vet, vetor_aux, tabela->qtd);
+    free(tabela->vet);
+    tabela->vet = vetor_aux;
+    tabela->tamanho = tabela->tamanho * multiplicador;
+  }
+
+  tabela->vet[tabela->qtd] = x;
+  tabela->qtd++;
+}
+
+void remover(tabela_dinamica * tabela) {
+
+};
+
+long int * aloca_vetor(long int qtd) {
+  int i = 0;
+  long int * novo_vetor;
+  novo_vetor = (long int *)malloc(sizeof(long int) * qtd);
+  for(i=0; i < qtd; i++) {
+    novo_vetor[i] = 1;
+  }
+
+  return novo_vetor;
+}
+
+void copia (long int * vetor1, long int *  vetor2, long int qtd) {
+  int i = 0;
+  for(i=0;i<qtd;i++) {
+    vetor2[i] = vetor1[i];
+  };
 }
