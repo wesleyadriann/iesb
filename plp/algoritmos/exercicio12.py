f7122d27d8a03d683af9bb146f2c0b19af73e61d
Wesley <wesleyadriann@gmail.com>
2020-05-11 20:59:09 -0300

    Adds exercicio12
    Signed-off-by: Wesley <wesleyadriann@gmail.com>

---
+++ plp/algoritmos/exercicio12.py
@@ -0,0 +1,37 @@
+
+# -*- coding: utf-8 -*-
+
+def main():
+    numeros = []
+    soma = 0
+    numero = -1
+
+    try:
+        while(numero is not 0):
+            try:
+                numero = int(input('Numero '))
+
+                if(numero > 100 or numero < -100):
+                    raise Exception('Número invalido, deve estar entre -100 e 100.')
+
+                numeros.append((pow(numero, 2)))
+                soma = sum(numeros)
+
+                print(f'Soma dos quadrados igual: {soma}')
+
+            except BaseException:
+                raise Exception('A entrada deve ser um numero inteiro.')
+
+            except OverflowError:
+                raise('Soma dos valores é muito grande.')
+
+        print(f'Soma dos quadrados igual: {soma}')
+
+    except Exception as error:
+        if(soma > 0):
+            print(f'Soma dos quadrados igual: {soma}')
+
+        print(f'{error}')
+
+if __name__ == '__main__':
+    main()
