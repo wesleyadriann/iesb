7c2feac66fc61e9497ebf34f7098252068226527
Wesley <wesleyadriann@gmail.com>
2020-06-19 20:30:39 -0300

    Adds karatsuba_ofman and multiplicacao
    Signed-off-by: Wesley <wesleyadriann@gmail.com>

---
+++ analise_de_algoritmos/algoritmos/karatsuba_ofman.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+int karatsuba_ofman(int u, int v, int n);
+
+int main() {
+  return 0;
+}
+
+int karatsuba_ofman(int u, int v, int n) {
+  if(n < 3) {
+    return u * v;
+  } else {
+    int m = 2/n;
+
+  }
+  return 0;
+}
