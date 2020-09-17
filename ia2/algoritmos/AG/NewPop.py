3b8d9c457a4c0bccbfaf149008bb24b4ccae52f8
Wesley Adriann <wesleyadriann@gmail.com>
2020-09-17 18:51:04 -0300

    Fixes newpop
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- ia2/algoritmos/AG/NewPop.py
+++ ia2/algoritmos/AG/NewPop.py
@@ -2,23 +2,21 @@
 # -*- coding: utf-8 -*-
 
 # nind - numero de individuos da populacao
-# comlim - matrix (ncrom x 2) contendo os limites superior e inferior para os cromossomos (parametros)
+# cromlim - matrix (ncrom x 2) contendo os limites superior e inferior para os cromossomos (parametros)
 # ncrom - numero de cromossomos (parametros) por individuo
 
 from random import randint
 
 class NewPop():
-
     def __init__(self, nind, cromlim):
         self.nind = nind
         self.cromlim = cromlim
-        self.new_ṕop = [[None]*len(cromlim)]*len(nind)
+        self.new_ṕop = [[None]*len(cromlim)]*nind
 
-    def gerar():
+    def gerar(self):
         ncrom = len(self.cromlim)
-        for i in range(len(self.nind)):
+        for i in range(self.nind):
             for j in range(ncrom):
                 inf = self.cromlim[j][0]
                 sup = self.cromlim[j][1]
-                self.new_pop[i][j] = randint(0, 2) * (sup-inf) + inf
-        return self.new_pop
+                self.new_ṕop[i][j] = randint(0, 1000) * (sup-inf) + inf
--- ia2/algoritmos/AG/__init__.py
+++ ia2/algoritmos/AG/__init__.py
@@ -1,10 +1,15 @@
 
 # -*- coding: utf-8 -*-
 
-import Code
-import Decode
-import NewPop
+# import Code
+# import Decode
+from NewPop import NewPop
 
 if __name__ == "__main__":
-    pop = NewPop(nind = )
+    pop_size = 3
+    cromlim = [[0, 255],[0, 255],[0, 255]]
+    pop = NewPop(pop_size, cromlim)
+    print(pop.new_ṕop)
+    pop.gerar()
+    print(pop.new_ṕop)
 
