c668743704e6cf5556a7b47fc0b74e436f2b0e58
Wesley Adriann <wesleyadriann@gmail.com>
2021-03-11 22:31:08 -0300

    Adds initial files to SIMPLE compiler
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

---
+++ compiladores/algoritmos/Compiler/Compiler.py
@@ -0,0 +1,9 @@
+
+# -*- coding: utf-8 -*-
+
+class Compiler():
+    def main(self):
+        pass
+
+if (__name__ == '__main__'):
+    Compiler().main()
---
+++ compiladores/algoritmos/Compiler/Token.py
@@ -0,0 +1,13 @@
+
+# -*- coding: utf-8 -*-
+
+class Token():
+    def __init__(self):
+        self.type = ''
+        self.table_id = ''
+        self.linha = ''
+        self.coluna
+
+    def __repr__(self):
+        return f'[{self.type}, {self.table_id}, ({self.linha}, {self.coluna})]'
+
