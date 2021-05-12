105910622dfb2e962a2557efaed5e2543dd14158
Wesley Adriann <wesleyadriann@gmail.com>
2021-05-12 00:23:18 -0300

    Adds open mpi docker
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- programacao_paralela/open_mpi_docker/master_entrypoint.sh
+++ programacao_paralela/open_mpi_docker/master_entrypoint.sh
@@ -1,2 +1,12 @@
 echo "MASTER ENTRYPOINT"
+sleep 2
+ping -c 3 slave
+echo "============================"
+sleep 2
+
+yes | ssh-keygen
+ssh-copy-id $USER@slave
+
+echo 'teste pasta compartilhada' > ~/openmpi/pasta_compartilhada/teste.txt
+
 tail -f /dev/null
--- programacao_paralela/open_mpi_docker/slave_entrypoint.sh
+++ programacao_paralela/open_mpi_docker/slave_entrypoint.sh
@@ -1,2 +1,15 @@
 echo "SLAVE ENTRYPOINT"
+sleep 2
+ping -c 3 master
+echo "============================"
+sleep 2
+
+yes | ssh-keygen
+ssh-copy-id $USER@master
+echo "============================"
+sleep 2
+
+sudo mount master:/home/$USER/openmpi/pasta_compartilhada ~/openmpi/pasta_compartilhada
+ls ~/openmpi/pasta_compartilhada
+
 tail -f /dev/null
